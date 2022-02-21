297f782582390975eaaf09985a2aab885df7e90c
Andrew Castro <andrew@Andrews-MacBook-Pro.local>
2022-02-21 14:01:28 -0500

    extending chakra theme to use outfit font family

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import theme from './theme'
+import '@fontsource/outfit'
 
 ReactDOM.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>,
   document.getElementById('root')
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+    fonts: {
+      heading: 'Outfit, sans-serif',
+      body: 'Outfit, sans-serif',
+    },
+  })
+  
+  export default theme
