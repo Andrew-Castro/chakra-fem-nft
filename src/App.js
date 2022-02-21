b40514b35f5f515cd34b8397f80b5c9414032fe7
Andrew Castro <andrew@Andrews-MacBook-Pro.local>
2022-02-21 11:02:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Center, Heading, Text, HStack, Divider, Image, Flex, Link } from '@chakra-ui/react'
+import avatar from './image-avatar.png'
+import equil from './image-equilibrium.jpg'
+import eth from './icon-ethereum.svg'
+import clock from './icon-clock.svg'
 
 function App() {
+
+  let textColor = "#8BACD9"
+  let hover = "#00FFF8"
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Center bg="#0D192C" h="100vh">
+        <Box bg="#15263F" color="white" borderRadius="2xl" p={6} w={350}>
+        <Image
+          src={equil}
+          borderRadius="8px"
+          marginBottom={6}
+          cursor="pointer"
         >
-          Learn React
-        </a>
-      </header>
+
+        </Image>
+        <Heading 
+          as="h2" 
+          fontSize="22px" 
+          mb={4}
+          cursor="pointer"
+          _hover={{
+            color: "#00FFF8"
+          }}>
+          Equilibrium #3429
+        </Heading>
+        <Text color={textColor} mb={6} fontSize="18px">
+          Our Equilibrium collection promotes balance and calm.
+        </Text>
+        <HStack justify="space-between">
+          <Flex align="center">
+            <Image
+              src={eth}
+              marginRight="6px"
+              height="18px"
+            >
+            </Image>
+            <Text color="#00FFF8" fontWeight="bold">
+              0.041 ETH
+            </Text>
+          </Flex>
+          <Flex align="center">
+            <Image
+                src={clock}
+                marginRight="6px"
+                height="16px"
+              >
+
+            </Image>
+            <Text color={textColor}>
+              3 days left
+            </Text>
+          </Flex>
+        </HStack>
+        <Divider borderColor="#2E405A" mt={6} mb={4}/>
+        <Flex>
+          <Image
+              border="1px"
+              borderColor="white"
+              borderRadius="50px"
+              boxSize='30px'
+              objectFit='cover'
+              src={avatar}
+              alt='Profile Picture'
+              marginRight={4}
+            />
+          <Text color={textColor}>
+            Creation of{' '}
+            <Link 
+              color='white' 
+              href='#'
+              _hover={{
+                color: "#00FFF8"
+              }}>
+              Jules Wyvern
+            </Link>
+          </Text>
+        </Flex>
+        </Box>
+      </Center>
     </div>
   );
 }
